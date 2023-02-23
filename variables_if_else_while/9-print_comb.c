b4599ce1e390d1e497e122a7bcc3d523e6e25376
root <ilyesmazouzz@gmail.com>
2023-02-23 03:09:15 -0800

    numbers

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Alwyas 0 (Success)
+*/
+
+int main(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar (c);
+		putchar (',');
+		putchar (' ');
+	}
+	putchar('\n');
+	return (0);
+}
