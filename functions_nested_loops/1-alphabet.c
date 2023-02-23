09c6a0a7191b89bec65aebadd251e6f0cbc43d3a
root <ilyesmazouzz@gmail.com>
2023-02-23 12:33:10 -0800

    alpabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+* main - Entry point
+*
+* Return: Alwyas 0 (Success)
+*/
+
+void print_alphabet(void);
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar (c);
+	putchar ('\n');
+	return (0);
+}
+
+
