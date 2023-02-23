9e6e006b7bf4529bf55a5e4309322428ebb5b348
root <ilyesmazouzz@gmail.com>
2023-02-23 00:58:48 -0800

    big and small alphabet

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Alwyas 0 (Success)
+*/
+
+	int main(void)
+{
+	char c, C;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	for (C = 'A'; C <= 'Z'; C++)
+	putchar (C);
+	putchar('\n');
+	return (0);
+}
