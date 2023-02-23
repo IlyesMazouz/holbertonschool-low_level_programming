9e9f46e8067b1bca9ba6da0bab22f98223a00425
root <ilyesmazouzz@gmail.com>
2023-02-23 03:12:19 -0800

    numbers

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -10,12 +10,13 @@ int main(void)
 {
 	int c;
 
-	for (c = '0'; c <= '9'; c++)
+	for (c = '0'; c <= '8'; c++)
 	{
 		putchar (c);
 		putchar (',');
 		putchar (' ');
 	}
+	putchar ('9');
 	putchar('\n');
 	return (0);
 }
