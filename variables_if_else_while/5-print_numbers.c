dff97072115e9e904491c236f89eef95f8294d9f
root <ilyesmazouzz@gmail.com>
2023-02-23 01:30:37 -0800

    print numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Alwyas 0 (Success)
+*/
+
+	int main(void)
+{
+	int n = 0;
+	while (n < 10)
+{
+	printf("%i", n);
+	n++;
+}
+	putchar ('\n');
+	return (0);
+}
