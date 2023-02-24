c16665fdef4e71ab65b2ec0490776c91b9c65293
root <ilyesmazouzz@gmail.com>
2023-02-24 10:56:33 -0800

    print alhpa x10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* print_alphabet - No desc
+*
+* Return: defaut
+*/
+
+void print_alphabet_x10(void)
+
+{
+	int c;
+	char a;
+
+	for (c = 0; c < 10; c++)
+		{
+		for (a = 'a'; a <= 'z'; a++)
+			_putchar(a);
+
+		_putchar('\n');
+	}
+}
