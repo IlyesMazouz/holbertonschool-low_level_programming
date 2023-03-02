9063b319bbcf3b9d193358f991daac80d5d0cc0e
root <ilyesmazouzz@gmail.com>
2023-03-02 12:19:29 -0800

    cd holbertonschool-low_level_programming/ and 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*print_most_numbers - print all numbers except 2 and 4
+*
+*/
+
+void print_most_numbers(void)
+{
+	int n = 0;
+
+	for (n = 48; n < 58; n++)
+	{
+		if (n != 50 && n != 52)
+		{
+			_putchar (n);
+		}
+	}
+	_putchar ('\n');
+}
