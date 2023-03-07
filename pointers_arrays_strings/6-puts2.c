9fbc107de9ad9f0a762710812ec70231b1373605
root <ilyesmazouzz@gmail.com>
2023-03-07 05:46:20 -0800

    printing pairs

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include "string.h"
+
+/**
+*puts2 - printing pair numbers only
+*@str: string
+*/
+
+void puts2(char *str)
+{
+	int n;
+	int len = strlen(str);
+
+	for (n = 0; n < len; n + = 2)
+		_putchar (str[n]);
+	_putchar ('\n');
+}
