ebf344e0ec64e965b695506062c40890886d9ba4
root <ilyesmazouzz@gmail.com>
2023-03-20 13:08:22 -0700

    comparing strings

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include "stddef.h"
+
+/**
+*_strpbrk - a function that searches a string for any of a set of bytes
+*@s: a string
+*@accept: a string
+*Return: value of s
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+	int a, b;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		for (b = 0; accept[b]; b++)
+		{
+			if (s[a] == accept[b])
+			{
+				return (s + a);
+	}
+		}
+			}
+	return (0);
+}
