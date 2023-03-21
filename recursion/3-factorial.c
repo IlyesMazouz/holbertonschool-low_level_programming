269994c5fcffb12b91cd7b3171c162a6092d1397
root <ilyesmazouzz@gmail.com>
2023-03-21 15:24:43 -0700

    my first factorial function

---
+++ recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*factorial - returning the factorial of a given number
+*@n: an integer
+*Return: value of n
+*/
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
