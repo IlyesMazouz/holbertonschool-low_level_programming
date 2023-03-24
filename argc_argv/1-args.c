c213cbe55e85cbad3939bdd40d734796ae709d44
root <ilyesmazouzz@gmail.com>
2023-03-24 06:14:04 -0700

    number of arguments

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+
+/**
+*main - counting the number of arguments in the programme
+*@argc: an int
+*@argv: an array
+*Return: (0)
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("the number of arguments is : %d\n", argc);
+	(void)argv;
+	return (0);
+}
