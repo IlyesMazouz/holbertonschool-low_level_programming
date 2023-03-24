b34d01ae1a0cc1d6a3d717b7e4a74e763bb59d31
root <ilyesmazouzz@gmail.com>
2023-03-24 06:07:08 -0700

    changing prog name

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,11 +1,15 @@
 #include "stdio.h"
 
 /**
+*main - printing the programme's name
+*@argc: an int
+*@argv: an array
+*Return: (0)
 */
 
 int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
-	printf("number of arguments is: %d\n", argc);
-	return(0);
+	(void)argc;
+	return (0);
 }
