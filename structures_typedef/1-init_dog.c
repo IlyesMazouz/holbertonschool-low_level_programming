a1f2c7fbe77b7bda791988b109fcceb205309639
root <ilyesmazouzz@gmail.com>
2023-03-27 02:50:46 -0700

    initializing variables

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,5 +1,4 @@
 #include "dog.h"
-#include "stdlib.h"
 
 /**
 *init_dog - initializing a variable of type struct dog
@@ -11,10 +10,7 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != NULL)
-	{
 		d->name = name;
 		d->age = age;
 		d->owner = owner;
-	}
 }
