8e6aaf411d6a2cfb3131cef62f2262be7fa659ab
root <ilyesmazouzz@gmail.com>
2023-03-27 02:07:04 -0700

    struct dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+*struct dog - defining a new structure named dog
+*@name: name of dog
+*@age: age of dog
+*@owner: owner of dog
+*/
+
 struct dog
 {
 	char *name;
