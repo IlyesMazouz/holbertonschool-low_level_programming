23ebf7765432b9e6c271d30ec8a9e90b5e322e61
root <ilyesmazouzz@gmail.com>
2023-03-27 05:38:29 -0700

    new dog type

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -15,8 +15,12 @@ struct dog
 	char *owner;
 };
 
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+dog_t my_dog(char *name, float age, char *owner);
 
 #endif
 
