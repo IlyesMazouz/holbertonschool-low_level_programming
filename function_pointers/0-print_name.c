72869392a256515753c1077ab7cf6e1c0f7cdf72
root <ilyesmazouzz@gmail.com>
2023-03-28 04:10:29 -0700

    calling fucntion to print

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include "stdlib.h"
 
 /**
 *print_name - calling functions to print a name
@@ -8,5 +9,10 @@
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (f == NULL || name == NULL)
+	{
+		return;
+	}
+
 	f(name);
 }
