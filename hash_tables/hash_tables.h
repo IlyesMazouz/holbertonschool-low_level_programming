ed0f90855f8b877de0899b299080d0410925ebc5
root <ilyesmazouzz@gmail.com>
2023-04-14 07:19:31 -0700

    header file

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -5,18 +5,19 @@
 #include "stdio.h"
 
 /**
- * struct hash_node_s - Node of a hash table
- *
- * @key: The key, string
- * The key is unique in the HashTable
- * @value: The value corresponding to a key
- * @next: A pointer to the next node of the List
- */
+*struct hash_node_s - Node of a hash table
+*
+*@key: The key, string
+*The key is unique in the HashTable
+*@value: The value corresponding to a key
+*@next: A pointer to the next node of the List
+*/
+
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -29,8 +30,8 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
