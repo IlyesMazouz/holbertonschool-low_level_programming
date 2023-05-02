d3cd0807df5bfc296bd3162d390cf15fa2b70d5c
root <ilyesmazouzz@gmail.com>
2023-05-02 10:18:53 -0700

    header file

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,6 +1,21 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+#include "stdio.h"
+#include "stdarg.h"
+
+/**
+*struct print - Struct print
+*@format_specifier: format specifier to print
+*@f: pointer to the corresponding function
+*/
+
+typedef struct printf
+{
+	char format_specifier;
+	void (*f)(va_list args);
+} _printf;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
